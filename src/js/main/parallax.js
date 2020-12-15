063c46cd7ba4f4a03c49af7abf40aef9b4996ca3
Goran <gospaso@gmail.com>
2020-12-15 03:04:54 -0500

    init parallax

--- src/js/main/parallax.js
+++ src/js/main/parallax.js
@@ -1,5 +1,3 @@
-window.addEventListener('scroll', event => {
-  const pos = window.scrollY
-  const el = document.querySelector('.pro-1 .parallax')
-  // el
-})
+import Rellax from 'rellax'
+
+const catchall = new Rellax('.rellax')
